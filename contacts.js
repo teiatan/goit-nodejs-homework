41fc9f2f964fa742dc1e75b9545750ef8778886a
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-11 23:16:44 +0300

    Initial commit

---
+++ contacts.js
@@ -0,0 +1,32 @@
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+
+const contacts = require("./db/contacts.json");
+
+function listContacts() {
+    // ...твой код
+  };
+  
+  function getContactById(contactId) {
+    // ...твой код
+  };
+  
+  function removeContact(contactId) {
+    // ...твой код
+  };
+  
+  function addContact(name, email, phone) {
+    // ...твой код
+  };
+
+  function test() {
+    console.log(contacts);
+  }
+
+module.exports = {
+    test
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require("fs/promises");
+
+
+//console.log(contacts);
+
+const test =  require("./contacts");
+console.log(test.test());
