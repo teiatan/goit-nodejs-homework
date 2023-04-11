47000a886f2e3ea153b60899ef7c1069b9b38863
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-11 23:19:32 +0300

    add import from contact to index

--- contacts.js
+++ contacts.js
@@ -28,5 +28,9 @@ function listContacts() {
   }
 
 module.exports = {
-    test
+    test,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
 }
--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const fs = require("fs/promises");
 
 //console.log(contacts);
 
-const test =  require("./contacts");
-console.log(test.test());
+const {test,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact} =  require("./contacts");
+
+    
+test();
